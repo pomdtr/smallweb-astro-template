eb174c0495334475c6b1aea054fdc4cd92288141
pomdtr <achille.lacoin@gmail.com>
2024-06-24 14:14:21 +0200

    update app build

--- app/dist/server/manifest_DBwvrDNm.mjs
+++
@@ -1,7 +0,0 @@
-import '@astrojs/internal-helpers/path';
-import 'cookie';
-export { m as manifest } from './chunks/_@astrojs-manifest_BqmI3rA-.mjs';
-import 'kleur/colors';
-import 'html-escaper';
-import 'clsx';
-import './chunks/astro/server_cMLEJ-TT.mjs';
